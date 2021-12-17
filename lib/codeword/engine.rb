c3d8f9974eb6b2943323c43cb9969eb22f1030fc
Dan Kim <itsdanya@gmail.com>
2021-12-17 19:51:57 +0300

    Remove generators config

--- lib/codeword/engine.rb
+++ lib/codeword/engine.rb
@@ -2,12 +2,6 @@ module Codeword
   class Engine < ::Rails::Engine
     isolate_namespace Codeword
 
-    config.generators do |g|
-      g.test_framework :rspec, fixture: false
-      g.assets false
-      g.helper false
-    end
-
     initializer 'codeword.app_controller' do |_app|
       ActiveSupport.on_load(:action_controller) { include Codeword }
     end
